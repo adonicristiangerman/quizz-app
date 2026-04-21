35f3533009d860b180196fc1c1e0853604c3352a
adonicristiangerman <adoni.cristiang@gmail.com>
2026-04-20 21:55:20 -0400

    I really don't want to keep working on this :v

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -10,6 +10,16 @@ export default function Question({
 }) {
   const [answer, setAnswer] = useState({ selectedAnswer: "", isCorrect: null });
 
+  let timer = 10000;
+
+  if (answer.selectedAnswer) {
+    timer = 1000;
+  }
+
+  if (answer.isCorrect !== null) {
+    timer = 2000;
+  }
+
   function handleSelectAnswer(answer) {
     setAnswer({
       selectedAnswer: answer,
@@ -36,7 +46,12 @@ export default function Question({
 
   return (
     <div id="question">
-      <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
+      <QuestionTimer
+        key={timer}
+        timeout={timer}
+        onTimeout={answer.selectedAnswer === "" ? onSkipAnswer : null}
+        mode={answerState}
+      />
       <h2>{QUESTIONS[questionIndex].text}</h2>
       <Answers
         answers={QUESTIONS[questionIndex].answers}
